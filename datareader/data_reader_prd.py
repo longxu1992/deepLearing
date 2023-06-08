ed65d6126cc94588a21c2bdd466110a6c5a8530e
long\xuzhilong <long>
2023-06-09 05:00:28 +0800

    添加继续学习2

--- datareader/data_reader_prd.py
+++ datareader/data_reader_prd.py
@@ -3,22 +3,22 @@ import numpy as np
 
 
 class DataReaderPrd:
-    def __init__(self, data_enum):
+    def __init__(self, data_enum, week_number, day_of_week):
         self.conn = sqlite3.connect(data_enum.db_path)
         self.cursor = self.conn.cursor()
         self.sql = data_enum.sql
         self.model_db = data_enum.name
+        self.week_number = week_number
+        self.day_of_week = day_of_week
 
     def get_batch(self):
         # self.cursor.execute(f'SELECT * FROM {self.table_name} LIMIT {batch_size} OFFSET {self.offset}')
         self.cursor.execute(f'''
-                {self.sql}
+                {self.sql} 
+                and  rq_week0 = {self.week_number} AND rq_day0 = {self.day_of_week}
         ''')
         result = self.cursor.fetchall()
         if len(result) == 0:
-            return None, None
+            return None
         result = np.array(result, dtype=np.float32)
-        X = result[:, :-1]
-        y = result[:, -1:]
-        y = np.where(y > 0, 1, 0)
-        return X, y
+        return result
--- model7/predict.py
+++ model7/predict.py
@@ -19,26 +19,31 @@ def predict_model(model, input_data):
 
 
 def main():
-    data_base = DataModelEnum.M7T1
-    model_base = DataModelEnum.M7D1
+    data_base = DataModelEnum.M7PRD
+    model_base = DataModelEnum.M7D3
     input_size = data_base.input_size
     hidden_size = 1024
     num_classes = 1
-    data_reader = DataReaderPrd(data_base)
+    data_reader = DataReaderPrd(data_base, 24, 5)
     model = MLP(input_size, hidden_size, num_classes)
 
     # Load model
     model.load_state_dict(torch.load(f'{model_base.name}model_epoch_900.pth'))
-    X, y = data_reader.get_batch()
+    X = data_reader.get_batch()
     # Here you should load your actual data
     input_data = torch.from_numpy(X.astype(np.float32)).to(device)
+    result = []
 
     predictions = predict_model(model, input_data)
     # Print input data and corresponding predictions
     for i in range(len(input_data)):
-        print('Input:', input_data[i].cpu().numpy().tolist())
-        print('Prediction:', 'Positive' if predictions[i] == 1 else 'Negative')
-        print()
+        input_info = input_data[i].cpu().numpy().tolist()
+        if predictions[i] == 1:
+            print('Input:', input_info)
+            result.append(input_info[-1])
+            print('Prediction:', 'Positive' if predictions[i] == 1 else 'Negative')
+            print()
+    print(result)
 
 
 if __name__ == '__main__':
