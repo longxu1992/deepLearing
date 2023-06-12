7649a092fe3fba13bb518b096b622f5a8b8c76ca
longxu1992 <51522771+longxu1992@users.noreply.github.com>
2023-06-13 07:01:45 +0800

    Create model8.py

---
+++ model8/model8.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, input_size=229, hidden_size=128, num_classes=5):  # Adjust input_size and num_classes
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, num_classes)
+        self.relu = nn.LeakyReLU()  # Changed to LeakyReLU
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)  # Pass through new hidden layer
+        out = self.fc3(out)
+        return out
